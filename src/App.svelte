84e9f8072eafdeb9542b5c339ddf1844603db4f7
lion-artiste <lionmusicofficiel@gmail.com>
2023-05-29 14:44:21 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import Waveform from './lib/Waveform.svelte'
+</script>
+
+<main>
+  <div>
+    <h1>Waveform</h1>
+    <Waveform/>
+  </div>
+
+  <div>The end</div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Waveform.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+import WaveSurfer from 'wavesurfer.js';
+import { onMount } from "svelte";
+
+let htmlElement;
+let wavesurfer;
+
+onMount(() => {
+  wavesurfer = WaveSurfer.create({
+    container: "#waveform",
+    waveColor: '#4F4A85',
+    progressColor: '#383351',
+    url: '/audio.mp3',
+})
+
+})
+
+</script>
+
+<div id="waveform" bind:this={htmlElement}></div>
