e9da1a6b7141505d8ff6920f79dc6f19743e7a97
lion-artiste <lionmusicofficiel@gmail.com>
2023-05-31 15:36:06 +0200

    1 file modified

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -8,8 +8,14 @@ export let task;
 export let state = "close";
 </script>
 
-<div class="bg-red-500 relative h-[80px]">
-  <div class="bg-white absolute t-0 left-0 w-full h-full border-2">
+<div class="relative h-[80px] flex flex-row">
+
+  <!-- Backgrounds -->
+  <div class="w-full bg-yellow-500"></div>
+  <div class="w-full bg-red-500"></div>
+
+  <!-- Main task -->
+  <div class="bg-white absolute t-0 left-10 w-full h-full border-2">
     <div class="">{task?.name}</div>
   </div>
 </div>
