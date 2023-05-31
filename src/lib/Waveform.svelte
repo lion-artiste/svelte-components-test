e063af368ad172f674b1547d8a7a92bd251eed58
lion-artiste <lionmusicofficiel@gmail.com>
2023-05-31 21:18:00 +0200

    1 file modified

--- src/lib/Waveform.svelte
+++ src/lib/Waveform.svelte
@@ -3,27 +3,52 @@ import WaveSurfer from 'wavesurfer.js';
 import { onMount } from "svelte";
 import songFile from "/song_test.mp3"
 
-export let baseColor = "#4F4A85";
-export let progressColor = "#383351";
+let COLORS = ["blue", "red"];
+let colorsTable = {
+  "blue": {
+    wave: "hsla(213, 76%, 55%, 1)",
+    progress: "hsla(213, 75%, 40%, 1)",
+    background: "hsla(213, 76%, 90%, 1)"
+  },
+  "red": {
+    wave: "hsla(0, 76%, 55%, 1)",
+    progress: "hsla(0, 75%, 40%, 1)",
+    background: "hsla(0, 76%, 90%, 1)"
+  }
+}
+
+let loaded = false;
+
+function getRandomColor() {
+  return COLORS[Math.floor(Math.random()*COLORS.length)];
+}
+
+export let playing = false;
+export let color: typeof COLORS[number] = getRandomColor();
+export let audio = "https://webaudioapi.com/samples/metering/sounds/chrono.mp3";
 
 let wavesurfer;
 
 onMount(() => {
   wavesurfer = WaveSurfer.create({
     container: "#waveform",
-    waveColor: baseColor,
-    progressColor,
-    url: songFile,
+    waveColor: colorsTable[color].wave,
+    progressColor: colorsTable[color].progress,
+    url: audio,
     barWidth: 2
-})
+  });
 
 
-wavesurfer.on('interaction', () => {
-  wavesurfer.play()
-})
+  wavesurfer.on('interaction', () => {
+    wavesurfer.play()
+  });
+
+  wavesurfer.on("ready", () => {
+    loaded = true;
+  });
 
 })
 
 </script>
 
-<div id="waveform"></div>
+<div class:animate-pulse={!loaded} id="waveform" style:background-color={colorsTable[color].background}></div>
